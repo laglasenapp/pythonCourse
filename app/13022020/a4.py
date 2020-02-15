2b3b37a3d571194da053618ecb7ffe1c6bd377f7
Luiz Glasenapp <luizglasenapp@gmail.com>
2020-02-14 21:45:25 -0300

    Aula de sexta-feira

--- app/13022020/a4.py
+++ app/13022020/a4.py
@@ -1,8 +1,12 @@
-from flask import Flask, render_template
-
+from flask import Flask, render_template, redirect, request
+import os
 nome = 'Site Flask 2'
 
-lista_empresas = ['Proway','Hbsis','T-Systems','Senior','Philips']
+#lista_empresas = ['Proway','Hbsis','T-Systems','Senior','Philips']
+
+dicionario_empresa = {}
+
+firstTime = False
 
 #name - nome do arquivo - sintaxe exigida pelo Flask
 app = Flask(__name__)
@@ -13,11 +17,29 @@ def inicio():
 
 @app.route('/empresa')
 def empresa():
-    return render_template('empresa.html', titulo=nome, empresas=lista_empresas)
+    global firstTime
+    if not firstTime :
+        ler_arquivo()
+        firstTime = True
+    return render_template('empresa.html', titulo=nome, empresas=dicionario_empresa)
 
 @app.route('/bezi')
 def bezi():
     return 'O Lubi ama a Bezi '
+
+@app.route('/salvar_empresa', methods = ['POST'])
+def salvar_empresa():
+    empresa = request.form.get('empresa')
+    ti_param = request.form.get('ti')
+    ti = ti_param == 'on'
+    dicionario_empresa.update({empresa : ti})
+    gravar_arquivo()
+    return redirect('/empresa')
+
+@app.route('/cadastrar_empresa')
+def cadastrar_empresa_tela():
+    return render_template('cadastrar_empresa.html')
+
 
 @app.route('/usuario')
 def usuario():
@@ -31,5 +53,27 @@ def usuario():
             <h1>Hello, ''' + user['username'] + '''!</h1>
         </body>
     </html>'''
+
+def  ler_arquivo():
+    try:
+        with open('dados.txt','r') as arquivo:
+            for l in arquivo:
+                linha = l.strip()
+                array = linha.split(';')
+                empresa = array[0]
+                ti = array[1]
+                dicionario_empresa.update({empresa:ti})
+    except Exception as e:
+        print(e)
+    
+
+def gravar_arquivo():
+    try:
+        os.remove('dados.txt')
+    except:
+        print('Arquivo não existe')
+    with open('dados.txt','w') as arquivo:
+        for x in dicionario_empresa:
+            arquivo.write(f'{x};{dicionario_empresa[x]}\n')
 
 app.run(debug=True)
