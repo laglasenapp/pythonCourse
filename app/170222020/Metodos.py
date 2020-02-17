4b61343966cbde903c11f6dac5adea271b200bf6
Luiz Glasenapp <luizglasenapp@gmail.com>
2020-02-17 19:04:03 -0300

    Revisão de métodos

---
+++ app/170222020/Metodos.py
@@ -0,0 +1,18 @@
+def soma(numero1, numero2):
+    resultado = numero1 + numero2
+    return resultado
+
+res = soma(10,20)
+print(res)
+
+def subtracao(n1, n2, n3):
+    return n1 - n2 - n3
+
+res2 = subtracao(n3=10, n2=30, n1=90);
+print(res2)
+
+def multiplicacao(n1, n2, n3=1):
+    return n1 * n2 * n3
+
+res3 = multiplicacao(5,2)
+print(res3)
